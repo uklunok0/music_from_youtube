dec9e9b857d127239481cf0491c58760b297631f
uklunok0 <habhome@yahoo.com>
2023-06-06 07:36:41 +0300

    add parse link to youtube for get name file

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const bodyParser = require("body-parser");
 const ytdl = require("ytdl-core");
 const ffmpeg = require("fluent-ffmpeg");
 const readline = require("readline");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 const app = express();
 const port = 3000;
 
 //const url = "https://www.youtube.com/watch?v=UMER76w9ew8";
+
+let linkYoutube = "";
 
 app.get("/", (req, res) => {
   const form = `
@@ -24,27 +28,46 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post("/", (req, res) => {
-  let link = req.body.name;
-  console.log(link); // содержит введённые данные в форму
+  let link = req.body.name; // содержит введённые данные в форму
+  console.log(link);
   let videoId = ytdl.getURLVideoID(link);
   console.log("ID video: ", videoId);
 
-  let stream = ytdl(link, { quality: "highestaudio" });
-  ffmpeg.setFfmpegPath("C:/JS-PROGECTS/mp3_from_youtube/ffmpeg/bin/ffmpeg.exe");
-  let start = Date.now();
-  ffmpeg(stream)
-    .audioBitrate(320)
-    .save(
-      "C:/JS-PROGECTS/mp3_from_youtube/tmp_from_youtube/" + `${videoId}.mp3`
-    )
-    .on("progress", (p) => {
-      readline.cursorTo(process.stdout, 0);
-      process.stdout.write(`${p.targetSize}kb downloaded`);
-    })
-    .on("end", () => {
-      console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
-      res.send(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
-    });
+  const getTitle = async (link) => {
+    // ф-ция получения названия файла
+    try {
+      const response = await axios.get(link);
+      const $ = cheerio.load(response.data);
+      linkYoutube = $("title").text().replace(" - YouTube", "");
+      return linkYoutube;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  getTitle(link).then((linkYoutube) => console.log(linkYoutube)); // вывод названия в консоль
+
+  setTimeout(function () {
+    // задержка обработки запроса
+    let stream = ytdl(link, { quality: "highestaudio" }); // получить видеопоток по ссылке
+    ffmpeg.setFfmpegPath(
+      "C:/JS-PROGECTS/mp3_from_youtube/ffmpeg/bin/ffmpeg.exe"
+    );
+    let start = Date.now();
+    ffmpeg(stream) // преобразование видеопотока в *mp3
+      .audioBitrate(320)
+      .save(
+        "C:/JS-PROGECTS/mp3_from_youtube/tmp_from_youtube/" +
+          `${linkYoutube}.mp3`
+      )
+      .on("progress", (p) => {
+        readline.cursorTo(process.stdout, 0); // перемещает курсор в начало строки
+        process.stdout.write(`${p.targetSize}kb downloaded`);
+      })
+      .on("end", () => {
+        console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
+        res.send(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
+      });
+  }, 2000);
 });
 
 app.listen(port, () => {
